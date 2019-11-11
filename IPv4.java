99ae4ea3aa8063407c452a966f6f5af12d2292c1
ViniciusAz <viniciussazevedo@gmail.com>
2019-11-11 10:28:22 -0300

    ip pronto

--- IPv4.java
+++ IPv4.java
@@ -6,8 +6,26 @@ public class IPv4 {
 
     public IPv4(String ip) {
         this.ip = ip;
-        
         //Fazer o split da string e tratamento, etc.
+        String[] array = ip.split("\\/");
+        cidr = Integer.parseInt(array[1]);
+        array = array[0].split("\\.");
+        endereco = (Integer.parseInt(array[0]) << 24)
+                 + (Integer.parseInt(array[1]) << 16)
+                 + (Integer.parseInt(array[2]) << 8)
+                 + Integer.parseInt(array[3]);
+    }
+
+    public String getIp() { return ip; }
+    public int getCidr() { return cidr; }
+    public int getEndereco() { return endereco; }
+
+    public String toString() {
+        return (
+            "Endereço IP : " + ip +
+            "\nCidr        : " + cidr +
+            "\nIP Binario  : " + endereco
+        );
     }
 
 }
