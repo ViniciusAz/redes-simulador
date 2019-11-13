7f917f97793dbfda34ed50d2995a4aa4604c3d26
ViniciusAz <viniciussazevedo@gmail.com>
2019-11-13 10:20:23 -0300

    Pacote construido

--- Pacote.java
+++ Pacote.java
@@ -3,10 +3,28 @@ public class Pacote {
     private IPv4 origem;
     private IPv4 destino;
     private int ttl;
-    private int temProximo; //1 = tem, 0 = ultimo
+    private boolean temProximo; //1 = tem, 0 = ultimo
     private int offset;
 
     private List<Rota> rota;
-
     
+    public Pacote(IPv4 origem, IPv4 destino, int ttl) {
+        this.origem = origem;
+        this.destino = destino;
+        this.ttl = ttl;
+    }
+    public Pacote(IPv4 origem, IPv4 destino, int ttl, boolean temProximo, int offset) {
+        this.origem = origem;
+        this.destino = destino;
+        this.ttl = ttl;
+        this.temProximo = temProximo;
+        this.offset = offset;
+    }
+
+    public IPv4 getOrigem() { return origem; }
+    public IPv4 getDestino() { return destino; }
+    public int getTtl() { return ttl; }
+    public boolean temProximo() { return temProximo; }
+    public int getOffset() { return offset; }
+    public void updateTtl() { ttl--; }
 }
