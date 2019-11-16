b1fa9add880be8d910efb6663d205b6aacc7ca50
ViniciusAz <viniciussazevedo@gmail.com>
2019-11-16 11:20:05 -0300

    ajustes leitura

--- Leitura.java
+++ Leitura.java
@@ -1,70 +1,63 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.list;
+import java.util.List;
 import java.util.ArrayList;
 
-public Leitura{
+public class Leitura {
     Scanner ler = new Scanner(System.in);
     FileReader arq;
     BufferedReader lerArq;
     ArrayList<Router> roteadores;
     ArrayList<Node> nodos;
-    public leitura(String txt){
-        arq        = new FileReader(txt);
-        larArq     = new BufferedReader(arq);
+
+    public Leitura(String arquivo) throws FileNotFoundException {
+        arq        = new FileReader(arquivo);
+        lerArq     = new BufferedReader(arq);
         roteadores = new ArrayList<Router>();
         nodos      = new ArrayList<Node>();         
     }
-    public lendoArquivo(){
-        boolean node        = false;
-        boolean router      = false;
-        boolean routertable = false;
 
+    public void lendoArquivo() {
+        boolean ehNode    = false;
+        boolean ehRouter  = false;
+        boolean ehRouterT = false;
 
         try {
             Scanner ler = new Scanner(System.in);
-            String linha = lerArq.readLine(); // Primeira Linha
+            String linha = " ";
             String[] aux;
-            if(linha.equals("#NODE")) node = true;
-
             while (linha != null) {
-                linha = lerArq.readLine(); // lê da segunda até a última linha
-                if(linha.equals("#ROUTER")){
-                    router = true;
-                    node = false;
-                    routertable = false;
-                }
-                else if(linha.equals("#ROUTERTABLE")){
-                    routertable = true;
-                    node = false;
-                    routertable = false;
-                }else{
-                    if(router){
+                linha = lerArq.readLine(); // lê a linha inteira
+                if(linha.equals("#NODE")) { ehNode = true; ehRouter = ehRouterT = false; }
+                else if(linha.equals("#ROUTER")) { ehRouter = true; ehNode = ehRouterT = false; }
+                else if(linha.equals("#ROUTERTABLE")) { ehRouterT = true; ehNode = ehRouter = false;
+                } else {
+                    if(ehRouter) {
                         aux = linha.split(",");
                         ArrayList<Interface> listaInterfaces = new ArrayList<Interface>();
-                        for(int i = 0; i < Integer.parseInt(aux[1]; i++)){
+                        for(int i = 0; i < Integer.parseInt(aux[1]); i++) {
                             //                                   ID        IP             MAC          MTU
-                            listaInterfaces.add(new Interface(("e"+i), aux[3*(3+i)], aux[2*(3+i)], aux[4*(3+i)]));
+                            listaInterfaces.add(new Interface(("e"+i), aux[3*(3+i)], aux[2*(3+i)], Integer.parseInt(aux[4*(3+i)])));
                         }
                         roteadores.add(new Router(aux[0], listaInterfaces, novaRouterTable));
-                            
 
-                    }else if(node){
+                    } else if(ehNode) {
 
-                    }else{
+                    } else {
 
                     }
                 }
             }
- 
             arq.close();
         } catch (IOException e) {
             System.err.printf("Erro na abertura do arquivo: %s.\n",
             e.getMessage());
         }
     }
-    public ArrayList<Router> getRoteadores(){return roteadores;}
-    public ArrayList<Nodes> getNodos(){return nodos;}
+
+    public ArrayList<Router> getRoteadores() { return roteadores; }
+    public ArrayList<Node> getNodos() { return nodos; }
 }
